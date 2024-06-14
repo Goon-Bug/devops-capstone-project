7e34b24b38f20d92487b3c2ab4b7c8e46234a8e2
Goon-Bug <goon-bug@hotmail.com>
2024-06-14 13:30:35 -0400

    list

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -139,3 +139,11 @@ class TestAccountService(TestCase):
         "It should not read an account that is not found"
         resp = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_list_all_accounts(self):
+        "It should list all accounts"
+        self._create_accounts(5)
+        resp = self.client.get(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 5)
