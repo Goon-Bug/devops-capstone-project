a87f3fe79649dca69663d91ef63a67a3ab988dad
Goon-Bug <goon-bug@hotmail.com>
2024-06-20 12:09:56 -0400

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -187,5 +187,11 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(resp.headers.get(key), value)
+
+    def test_cors_policy(self):
+        """It should test whether CORS policies are implemented"""
+        resp = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        self.assertEqual(resp.headers.get('Access-Control-Allow-Origin'), '*')
         
 
